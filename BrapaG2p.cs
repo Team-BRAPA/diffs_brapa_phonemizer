54add461fbaba96aca76dbbb11bc7aadd1f9d747
HAI-D <68165064+overdramatic@users.noreply.github.com>
2024-09-10 23:05:39 -0300

    Add files via upload

---
+++ BrapaG2p.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.ML.OnnxRuntime;
+using OpenUtau.Api;
+
+namespace OpenUtau.Core.G2p {
+    public class BrapaG2p : G2pPack {
+        private static readonly string[] graphemes = new string[] {
+            "", "", "", "", "a", "b", "c", "d", "e", "f", "g",
+            "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+            "s", "t", "u", "v", "w", "x", "y", "z", "á", "â",
+            "ã","à","ç","é","ê","í","ó","ô","õ","ú","è","ì",
+            "ñ","ò","ü","ũ","\'","-",
+        };
+
+        private static readonly string[] phonemes = new string[] {
+            "", "", "", "", "a", "ae", "an", "ax", "b", "ch", "d", "dj", "e",
+            "eh", "en", "f", "g", "h", "hr", "i", "i0", "in", "j", "k", "l", "lh",
+            "m", "n", "ng","nh","o","oh","on","p","r","rh","rr","rw","s","sh","t",
+            "u","u0","un","v","w","x","y","z",
+        };
+
+        private static object lockObj = new object();
+        private static Dictionary<string, int> graphemeIndexes;
+        private static IG2p dict;
+        private static InferenceSession session;
+        private static Dictionary<string, string[]> predCache = new Dictionary<string, string[]>();
+
+        public BrapaG2p() {
+            lock (lockObj) {
+                if (graphemeIndexes == null) {
+                    graphemeIndexes = graphemes
+                    .Skip(4)
+                    .Select((g, i) => Tuple.Create(g, i))
+                    .ToDictionary(t => t.Item1, t => t.Item2 + 4);
+                    var tuple = LoadPack(DiffSingerBrapaPhonemizer.Resources.g2p_brapa);
+                    dict = tuple.Item1;
+                    session = tuple.Item2;
+                }
+            }
+            GraphemeIndexes = graphemeIndexes;
+            Phonemes = phonemes;
+            Dict = dict;
+            Session = session;
+            PredCache = predCache;
+        }
+    }
+}
---
+++ DiffSingerBRAPAPhonemizer.cs
@@ -0,0 +1,22 @@
+﻿using OpenUtau.Api;
+using OpenUtau.Core.G2p;
+
+namespace OpenUtau.Core.DiffSinger
+{
+    [Phonemizer("DiffSinger BRAPA Phonemizer", "DIFFS BRAPA", language: "PT")]
+    public class DiffSingerBRAPAPhonemizer : DiffSingerG2pPhonemizer
+    {
+        protected override string GetDictionaryName() => "dsdict-brapa.yaml";
+        protected override IG2p LoadBaseG2p() => new BrapaG2p();
+        protected override string[] GetBaseG2pVowels() => new string[] {
+            "a", "ae", "an", "ax", "e", "eh", "en", "i", "i0","in",
+            "o", "oh", "on", "u", "u0", "un"
+        };
+
+        protected override string[] GetBaseG2pConsonants() => new string[] {
+            "b", "ch", "d", "dj", "f", "g", "h", "hr", "j", "k", "l", "lh", "m", "n",
+            "ng", "nh", "p", "r", "rh", "rr", "rw", "s", "sh", "t", "v", "w", "x",
+            "y","z"
+        };
+    }
+}
